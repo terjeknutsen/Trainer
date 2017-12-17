ab1ca98ae8e1dbf2b232c6ee5bb6a9a9648f0837
terje knutsen <terje.knutsen@protonmail.com>
2017-12-17 09:12:59 +0100

    gitignore

---
+++ PushupAndroid.UiTest/WhenStartup/GivenOneChallengeActive.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest.Android;
+
+namespace PushupAndroid.UiTest.WhenStartup
+{
+    [TestFixture]
+    public class GivenOneChallengeActive
+    {
+        AndroidApp app;
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp();
+        }
+        [Test]
+        public void Then_challenge_elements_should_show()
+        {
+            app.Invoke("OneChallengeActive");
+            app.Flash(c => c.Marked("day 5"));
+            app.Flash(c => c.Marked("Hundre pushups hver dag"));
+            //app.Flash(c => c.Marked("100"));
+            //app.Flash(c => c.Marked("420"));
+            //app.Flash(c => c.Marked("mon"));
+            //app.Flash(c => c.Marked("tue"));
+            //app.Flash(c => c.Marked("wed"));
+            //app.Flash(c => c.Marked("thu"));
+            //app.Flash(c => c.Marked("fri"));
+            //app.Flash(c => c.Marked("sat"));
+            //app.Flash(c => c.Marked("sun"));
+            //app.Flash(c => c.Marked("mage"));
+            //app.Flash(c => c.Marked("96cm"));
+            //app.Flash(c => c.Marked("8cm"));
+            app.Repl();
+        }
+    }
+}
---
+++ PushupAndroid.UiTest/WhenStartup/GivenOneWorkoutPending.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest.Android;
+
+namespace PushupAndroid.UiTest.WhenStartup
+{
+    [TestFixture]
+    public class GivenOneWorkoutPending
+    {
+        AndroidApp app;
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp();
+        }
+        [Test]
+        public void Then_workout_element_should_be_displayed()
+        {
+            app.Invoke("OneWorkoutPending","test");
+            app.Flash(c => c.Id("workout_reps")); ;
+            app.Flash(c => c.Id("workout_description")); ;
+            app.Flash(c => c.Id("executedBtn")); ;
+        }
+    }
+}
